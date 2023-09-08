d73836c58bb3442a20a5dcc02dfe275ff9a96589
Andy Williams <andy@andy.xyz>
2023-09-08 13:24:46 +0100

    Episode 2 code

--- gui.go
+++ gui.go
@@ -6,9 +6,16 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+type gui struct {
+	win   fyne.Window
+	title binding.String
+}
 
 func makeBanner() fyne.CanvasObject {
 	toolbar := widget.NewToolbar(
@@ -20,16 +27,38 @@ func makeBanner() fyne.CanvasObject {
 	return container.NewStack(toolbar, container.NewPadded(logo))
 }
 
-func makeGUI() fyne.CanvasObject {
+func (g *gui) makeGUI() fyne.CanvasObject {
 	top := makeBanner()
 	left := widget.NewLabel("Left")
 	right := widget.NewLabel("Right")
 
-	content := canvas.NewRectangle(color.Gray{Y: 0xee})
+	directory := widget.NewLabelWithData(g.title)
+	content := container.NewStack(canvas.NewRectangle(color.Gray{Y: 0xee}), directory)
 
 	dividers := [3]fyne.CanvasObject{
 		widget.NewSeparator(), widget.NewSeparator(), widget.NewSeparator(),
 	}
 	objs := []fyne.CanvasObject{content, top, left, right, dividers[0], dividers[1], dividers[2]}
 	return container.New(newFysionLayout(top, left, right, content, dividers), objs...)
+}
+
+func (g *gui) openProjectDialog() {
+	dialog.ShowFolderOpen(func(dir fyne.ListableURI, err error) {
+		if err != nil {
+			dialog.ShowError(err, g.win)
+			return
+		}
+		if dir == nil {
+			return
+		}
+
+		g.openProject(dir)
+	}, g.win)
+}
+
+func (g *gui) openProject(dir fyne.ListableURI) {
+	name := dir.Name()
+
+	g.win.SetTitle("Fysion App: " + name)
+	g.title.Set(name)
 }
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/storage"
 )
 
 func main() {
@@ -12,6 +18,45 @@ func main() {
 	w.SetPadded(false)
 	w.Resize(fyne.NewSize(1024, 768))
 
-	w.SetContent(makeGUI())
+	ui := &gui{win: w, title: binding.NewString()}
+	w.SetContent(ui.makeGUI())
+	w.SetMainMenu(ui.makeMenu())
+	ui.title.AddListener(binding.NewDataListener(func() {
+		name, _ := ui.title.Get()
+		w.SetTitle("Fysion App: " + name)
+	}))
+
+	flag.Usage = func() {
+		fmt.Println("Usage: fysion [project directory]")
+	}
+	flag.Parse()
+	if len(flag.Args()) > 0 {
+		dirPath := flag.Args()[0]
+		dirPath, err := filepath.Abs(dirPath)
+		if err != nil {
+			fmt.Println("Error resolving project path", err)
+			return
+		}
+
+		dirURI := storage.NewFileURI(dirPath)
+		dir, err := storage.ListerForURI(dirURI)
+		if err != nil {
+			fmt.Println("Error opening project", err)
+			return
+		}
+
+		ui.openProject(dir)
+	} else {
+		ui.openProjectDialog()
+	}
+
 	w.ShowAndRun()
+}
+
+func (g *gui) makeMenu() *fyne.MainMenu {
+	file := fyne.NewMenu("File",
+		fyne.NewMenuItem("Open Project", g.openProjectDialog),
+	)
+
+	return fyne.NewMainMenu(file)
 }
